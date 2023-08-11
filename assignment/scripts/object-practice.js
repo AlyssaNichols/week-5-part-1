0c51276fc972df5e11e2b35c5c2d817ad4e41fca
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-08-11 14:09:28 -0500

    all completed anad ready to turn in, double checked everything and made sure I documented well

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -18,26 +18,25 @@ let me = {
 };
 console.log('A little about me:', me);
 
+
 /* 2. Accessing object properties.
   - Create a variable called fullName 
   - Use the firstName & lastName properties of the object you 
     created above to set its value. 
   - Console.log fullName
 */
-
 const fullName = me.firstName + ' ' + me.lastName;
-
 console.log('My full name is', fullName);
 
-// just console logging the array to make sure the next console logs line up
 
+// just console logging the array to make sure the next console logs line up
 console.log('My three favorite foods are:', me.favThreeFoods);
+
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
 console.log('The first food item on my list of favorite foods is', me.favThreeFoods[0]);
 console.log('The last food item on my list of favorite foods is', me.favThreeFoods[me.favThreeFoods.length -1]);
 
@@ -48,9 +47,7 @@ console.log('The last food item on my list of favorite foods is', me.favThreeFoo
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
-
 console.log('my shoe count is', me.shoeCount);
-
 console.log('I got a new pair! Now my shoe count is', me.shoeCount + 1);
 
 
@@ -60,9 +57,6 @@ console.log('I got a new pair! Now my shoe count is', me.shoeCount + 1);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
-
 me.favoriteColor = 'Lavender';
-
 console.log('Added my favorite color', me);
-
 console.log('my favorite color is', me.favoriteColor)
